b2ca64d4e2ce2594f7d631b3cc92d77f1ef96b90
Oliver <o.lucas@live.com>
2015-02-06 13:30:56 +0000

    first two tests passing

--- lib/questions.rb
+++ lib/questions.rb
@@ -1,10 +1,11 @@
 # keep only the elements that start with an a
 def select_elements_starting_with_a(array)
-  
+  array.map{|word| word if word.start_with?("a")}.compact
 end
 
 # keep only the elements that start with a vowel
 def select_elements_starting_with_vowel(array)
+  array.map{|word| word if word.start_with?("a", "i", "o", "u")}.compact
 end
 
 # remove instances of nil (but NOT false) from an array
