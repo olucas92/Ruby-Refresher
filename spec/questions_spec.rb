63b7c2df823dafa089f5869b95e63ef8ede69e3a
Oliver <o.lucas@live.com>
2015-02-10 16:25:05 +0000

    and another one

--- spec/questions_spec.rb
+++ spec/questions_spec.rb
@@ -131,10 +131,10 @@ describe 'the Friday test :)' do
     expect(n).to eq({'b' => 'a', 'd' => 'c'})
   end
 
-  # it 'add_together_keys_and_values' do
-  #   n = add_together_keys_and_values({1 => 1, 2 => 2})
-  #   expect(n).to eq 6
-  # end
+  it 'add_together_keys_and_values' do
+    n = add_together_keys_and_values({1 => 1, 2 => 2})
+    expect(n).to eq 6
+  end
 
   # it 'remove_capital_letters_from_string' do
   #   n = remove_capital_letters_from_string 'Hello JohnDoe'
